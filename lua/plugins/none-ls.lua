c20fcf7831556265050c8d6ebb0033d089cbd8b3
Michael Cordell <mike@mikecordell.com>
2024-05-31 09:16:35 -0700

    Update packages

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,7 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -16,6 +16,20 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.rubocop.with({
+        command = "bundle",
+        args = vim.list_extend(
+          { "exec", "rubocop" },
+          null_ls.builtins.formatting.rubocop._opts.args
+        ),
+      }),
+      null_ls.builtins.diagnostics.rubocop.with({
+        command = "bundle",
+        args = vim.list_extend(
+          { "exec", "rubocop" },
+          null_ls.builtins.diagnostics.rubocop._opts.args
+        ),
+      })
     }
     return config -- return final config table
   end,
