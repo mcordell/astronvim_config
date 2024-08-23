7a43a0e12f62dacad2cd24b6e38b8bfd817a6073
Michael Cordell <mike@mikecordell.com>
2024-08-23 15:49:49 -0700

    Add obsidian plugins

---
+++ lua/plugins/obsidian-bridge.lua
@@ -0,0 +1,17 @@
+return {
+  dir="~/code/lua/obsidian-bridge.nvim/",
+  event = {
+    "BufReadPre /Users/michael/Documents/Obsidian Vault/*.md",
+    "BufNewFile /Users/michael/Documents/Obsidian Vault/*.md",
+  },
+  lazy = true,
+  opts = {
+        obsidian_server_address = "http://127.0.0.1:27123",
+        scroll_sync = false -- See "Sync of buffer scrolling" section below
+
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  }
+}
