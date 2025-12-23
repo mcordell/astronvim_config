5271c2a7ab11393d9498021d974634d5322a94b8
Michael Cordell <mike@mikecordell.com>
2025-12-23 15:14:36 -0800

    Update obsidian plugin

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,19 +1,12 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
- -- ft = "markdown",
+  "obsidian-nvim/obsidian.nvim",
+  version = "3.14.7", -- recommended, use latest release instead of latest commit
+  ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  event = {
-    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    -- refer to `:h file-pattern` for more examples
-    "BufReadPre /Users/michael/Documents/Obsidian Vault/*.md",
-    "BufNewFile /Users/michael/Documents/Obsidian Vault/*.md",
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
+    legacy_commands = false,
     workspaces = {
       {
         name = "main",
@@ -24,5 +17,5 @@ return {
   config = function(_, opts)
     vim.wo.conceallevel = 2
     require("obsidian").setup(opts)
-  end
+  end,
 }
