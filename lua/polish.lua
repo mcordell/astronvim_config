6ca0bfa957b92f88d03ab9dfc10d18057de67f68
Michael Cordell <mike@mikecordell.com>
2026-02-10 12:53:17 -0800

    Fix file picker path being hard to read in snacks

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,11 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- Base path text (readable but not “matchy”)
+vim.api.nvim_set_hl(0, "SnacksPickerPath", { fg = "#D8DEE9" })
+vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#D8DEE9" })
 
+-- Matched characters (make them pop)
+vim.api.nvim_set_hl(0, "SnacksPickerMatch", { fg = "#EBCB8B", bold = true })
+vim.api.nvim_set_hl(0, "SnacksPickerMatchBold", { fg = "#EBCB8B", bold = true }) -- some versions use this
+vim.api.nvim_set_hl(0, "SnacksPickerMatchUnderline", { fg = "#EBCB8B", underline = true })
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
